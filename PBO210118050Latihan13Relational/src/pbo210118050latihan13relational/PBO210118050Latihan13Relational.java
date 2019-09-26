75fd803583b8856cdb7701aea46826c4c2abf942
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 20:15:49 +0700

    Add files via upload

---
+++ PBO210118050Latihan13Relational/src/pbo210118050latihan13relational/PBO210118050Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118050latihan13relational;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan penghitungan Relational
+ */
+public class PBO210118050Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bi11 = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bi11 == bi12));
+        System.out.println("a != b = " + (bi11 != bi12));
+        System.out.println("a > b = " + (bi11 > bi12));
+        System.out.println("a < b = " + (bi11 < bi12));
+        System.out.println("a >= b = " + (bi12 >= bi11));
+        System.out.println("a <= b = " + (bi12 <= bi11));
+        
+    }
+    
+}
